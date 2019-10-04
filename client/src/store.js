3852fba5e4817d47b5b0a2bd03fd67f87c0afe75
MasonCable <masoncablelsc@gmail.com>
2019-10-03 22:44:28 -0700

    added the store, and the initial reducers

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,20 @@
-import { createStore } from 'redux'
+// Use combineReducers for when we have to handle multiple states
+// We can import
+import { createStore, combineReducers } from 'redux'
 
-const rootReducer = () => {
-    return 'Store testing'
+const initialState = {
+    recipes: []
 }
 
-const store = createStore(rootReducer)
+export const testReducer = (state = initialState, action) => {
+    switch (action.type) {
+            case 'BUILD_RECIPES' :
+                return {...state, recipes: action.payload}
+            default:
+                return state
+        }
+}
+
+const store = createStore(testReducer)
 
 export default store
